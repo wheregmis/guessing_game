91ff7de65d61988728e1bc68414212fdca15606d
Sabin Regmi <xawbeenregmi@gmail.com>
2022-07-29 19:43:26 -0400

    Error Handling

--- src/main.rs
+++ src/main.rs
@@ -5,21 +5,30 @@ use std::io;
 fn main() {
     println!("Welcome to guessing game !!");
 
-    let secret_number = rand::thread_rng().gen_range(1..=10);
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("Pleas input a number");
+    loop {
+        println!("Pleas input a number");
 
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess).expect("Failed to read");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("Failed to read");
 
-    println!("You have guessed: {guess}");
-
-    // parsing integer from String
-    let guess: u32 = guess.trim().parse().expect("Please insert a number");
-    // Using match to compare
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too Small"),
-        Ordering::Greater => println!("Too Big"),
-        Ordering::Equal => println!("You win"),
+        // parsing integer from String
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Your input was not a number");
+                continue;
+            }
+        };
+        // Using match to compare
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too Small"),
+            Ordering::Greater => println!("Too Big"),
+            Ordering::Equal => {
+                println!("You win");
+                break;
+            }
+        }
     }
 }
